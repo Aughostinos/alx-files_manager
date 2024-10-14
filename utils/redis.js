43c7679907c7fe67a5dccae573738e70e70d862a
Augustinos <augustinos.nabil.com>
2024-10-14 01:49:18 -0700

    fix code style

--- utils/redis.js
+++ utils/redis.js
@@ -1,43 +1,43 @@
 import redis from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (err) => {
-            console.error(`Redis client error: ${err}`);
-        });
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.error(`Redis client error: ${err}`);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, reply) => {
-                if (err) reject(err);
-                resolve(reply);
-            });
-        });
-    }
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) reject(err);
+        resolve(reply);
+      });
+    });
+  }
 
-    async set(key, value, duration) {
-        return new Promise((resolve, reject) => {
-            this.client.setex(key, duration, value, (err) => {
-                if (err) reject(err);
-                resolve(true);
-            });
-        });
-    }
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
+  }
 
-    async del(key) {
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (err) => {
-                if (err) reject(err);
-                resolve(true);
-            });
-        });
-    }
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
