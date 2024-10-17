da94f7f412243edd610107ed744ccb5e13bcfe7a
Aughostinos <augustinos.nabil@gmail.com>
2024-10-17 05:40:29 +0300

    task 8

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { promises as fs } from 'fs';
 import { join as pathJoin } from 'path';
 import { ObjectId } from 'mongodb';
+import mime from 'mime-types';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -73,17 +74,17 @@ class FilesController {
         parentId,
       });
     } else {
-        const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-        await fs.mkdir(folderPath, { recursive: true });
+      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+      await fs.mkdir(folderPath, { recursive: true });
 
-        const localPath = pathJoin(folderPath, uuidv4());
-        const fileData = Buffer.from(data, 'base64');
+      const localPath = pathJoin(folderPath, uuidv4());
+      const fileData = Buffer.from(data, 'base64');
 
-        try {
-          await fs.writeFile(localPath, fileData);
-        } catch (error) {
-          return res.status(500).json({ error: 'Error saving file' });
-        }
+      try {
+        await fs.writeFile(localPath, fileData);
+      } catch (error) {
+        return res.status(500).json({ error: 'Error saving file' });
+      }
 
       fileDocument.localPath = localPath;
 
@@ -269,6 +270,44 @@ class FilesController {
     };
 
     return res.status(200).json(fileResponse);
+  }
+
+  static async getFile(req, res) {
+    const fileId = req.params.id;
+
+    if (!ObjectId.isValid(fileId)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const file = await dbClient.db.collection('files').findOne({
+      _id: ObjectId(fileId),
+    });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    if (!file.isPublic) {
+      const token = req.headers['x-token'];
+      const userId = await redisClient.get(`auth_${token}`);
+
+      if (!userId || userId.toString() !== file.userId.toString()) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+    }
+
+    if (file.type === 'folder') {
+      return res.status(400).json({ error: "A folder doesn't have content" });
+    }
+
+    try {
+      const fileContent = await fs.readFile(file.localPath);
+      const mimeType = mime.contentType(file.name) || 'application/octet-stream';
+      res.setHeader('Content-Type', mimeType);
+      return res.status(200).send(fileContent);
+    } catch (err) {
+      return res.status(404).json({ error: 'Not found' });
+    }
   }
 }
 
